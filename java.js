4a9cdfe0e9f766199301c5955fba4ea4d0a6ac4a
erobb11 <48133916+erobb11@users.noreply.github.com>
2019-03-15 11:42:25 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,49 +1,9 @@
-/*var xmlhttp = new XMLHttpRequest();
-var url = "news.json";
-
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' + 
-        arr[i].display + '</a><br>';
-    }
-    document.getElementById("id01").innerHTML = out;
-}
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//usage:
-readTextFile("news.json", function(text){
-    var data = JSON.parse(text);
-    console.log(data);
-});*/
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var news = JSON.parse(this.responseText);
     document.getElementById("news1").innerHTML = news.news1;
-      console.log("Done2");
+      console.log(news.news1);
   }
 };
 xmlhttp.open("GET", "news.json", true);
