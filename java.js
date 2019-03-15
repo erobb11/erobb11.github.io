13a94ebefbb0d96afae7037d34ca5eefb8d9b521
erobb11 <48133916+erobb11@users.noreply.github.com>
2019-03-15 07:32:15 -0500

    Update java.js

--- java.js
+++ java.js
@@ -42,7 +42,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var news = JSON.parse(this.responseText);
-    document.getElementById("news").innerHTML = news.news[1];
+    document.getElementById("news").innerHTML = news.news1;
       console.log("Done");
   }
 };
